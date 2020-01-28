98b86def64bd09827eb9c4c8ea85ba3fd8c3a4cb
RGDZ <rgdz.gzu@qq.com>
2020-01-28 23:39:32 +0800

    配置gunicorn日志文件

--- config.py
+++ config.py
@@ -19,3 +19,9 @@ threads = 4
 worker_class = 'gevent'
 # 设置最大并发量
 worker_connections = 2000
+
+# 设置访问日志和错误信息日志路径
+accesslog = './log/gunicorn_acess.log'
+errorlog = './log/gunicorn_error.log'
+# 设置日志记录水平
+loglevel = 'warning'
