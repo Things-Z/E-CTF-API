16757fb4d5bf6e8c31b11b6f2d61d1ff872f3790
RGDZ <rgdz.gzu@qq.com>
2020-01-28 23:26:18 +0800

    更新 gunicorn配置文件

--- config.py
+++ config.py
@@ -11,6 +11,11 @@
 '''
 
 bind = "0.0.0.0:8080"
-timeout = 5
-workers = 4
-threads = 1000
+# 并行工作进程数
+workers = 3
+# 指定每个工作者的线程数
+threads = 4
+# 工作模式协程
+worker_class = 'gevent'
+# 设置最大并发量
+worker_connections = 2000
