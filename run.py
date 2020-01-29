b435811036022190b798c4a81241c76f636eb11f
RGDZ <rgdz.gzu@qq.com>
2020-01-29 12:34:42 +0800

    删除多余文件

--- run.py
+++ run.py
@@ -13,6 +13,6 @@
 from app import app
 
 if __name__ == "__main__":
-    from werkzeug.contrib.fixers import ProxyFix
+    from werkzeug.middleware.proxy_fix import ProxyFix
     app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run()
